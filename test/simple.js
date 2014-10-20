b75d02ee842823f1e728ce48e96ebc283fd84b5f
James Nylen <jnylen@gmail.com>
2014-10-20 15:58:28 -0500

    Add automated tests
    Using substack/tape as the test reporter because unlike the bundled
    version of tap, it exits with a non-zero code if any errors occur.

--- test/simple.js
+++ test/simple.js
@@ -6,7 +6,9 @@ test('simple test', function (t) {
   console.error('this should go to stderr');
   t.ok(true);
   t.equal(true, true);
-  t.end();
+  setTimeout(function() {
+    t.end();
+  }, 100);
 });
 
 
@@ -16,20 +18,26 @@ test('another simple test', function (t) {
   console.error('another log to stderr');
   t.ok(true);
   t.equal('foo', 'foo');
-  t.end();
+  setTimeout(function() {
+    t.end();
+  }, 100);
 });
 
 test('showing some deeper stderr logging by inspect on tap.test', function (t) {
   console.error('on stderr:', t.conf);
   t.ok(true);
   t.equal('foo', 'foo');
-  t.end();
+  setTimeout(function() {
+    t.end();
+  }, 100);
 });
 
 test('showing some deeper stdout logging by inspect on tap.test', function (t) {
   console.log('on stdout:', t.conf);
   t.ok(true);
   t.equal('foo', 'foo');
-  t.end();
+  setTimeout(function() {
+    t.end();
+  }, 100);
 });
 
